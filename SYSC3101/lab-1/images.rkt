0d1ef2c1054a8134fd6bf0e9b446f9bc6364d31e
cartieraidan <cartieraidan@gmail.com>
2026-01-05 14:48:31 -0500

    Final

--- SYSC3101/lab-1/images.rkt
+++ SYSC3101/lab-1/images.rkt
@@ -4,14 +4,19 @@
 (define a-blue-circle (circle 50 "solid" "blue")) ; part 2
 (define outlined-square (rectangle 100 100 "outline" "red")) ; part 3
 (define outlined-circle (circle 50 "outline" "blue")) ; part 3
-(define image-1 (beside (rectangle 100 100 "solid" "red")
+(define row-of-squares (beside (rectangle 100 100 "solid" "red")
                         (rectangle 100 100 "solid" "blue")
                         (rectangle 100 100 "solid" "green"))) ; part 4
-(define image-2 (above (rectangle 100 100 "solid" "red")
+(define column-of-squares (above (rectangle 100 100 "solid" "red")
                         (rectangle 100 100 "solid" "blue")
                         (rectangle 100 100 "solid" "green"))) ; part 4
-(define image-3 (overlay/align "middle" "middle"
+(define nested-squares (overlay/align "middle" "middle"
                                (rectangle 20 20 "solid" "black")
                                (rectangle 40 40 "solid" "green")
                                (rectangle 60 60 "solid" "blue")
                                (rectangle 80 80 "solid" "red"))) ; part 4
+(define rotated-square (overlay/align "middle" "middle"
+                               (rotate 45 (rectangle 20 20 "solid" "black"))
+                               (rotate 45 (rectangle 40 40 "solid" "green"))
+                               (rotate 45 (rectangle 60 60 "solid" "blue"))
+                               (rotate 45 (rectangle 80 80 "solid" "red")))) ; part 5   
