2395ac6fd4d02c42b4349c258b0c692b6f18caf1
cartieraidan <cartieraidan@gmail.com>
2026-01-12 17:44:08 -0500

    Refactor Technician class and remove ObservableTech

--- SYSC3303/lab1/Technician.java
+++ SYSC3303/lab1/Technician.java
@@ -1,29 +1,64 @@
+import java.util.ArrayList;
+
 /**
  *
  *
  * @author Aidan Cartier
  * @version Jan 12, 2026
  */
-public class Technician extends Thread implements ObservableTech {
+public class Technician extends Thread  {
+
+    private boolean scheduled = false;
+    private Components inheritedComponent;
+    private final Agent agent;
+    private final Object lock;
+
+    public Technician(Components inheritedComponent, Agent agent) {
+        this.inheritedComponent = inheritedComponent;
+        this.agent = agent;
+        this.lock = agent.getLock();
+    }
+
+    public void setScheduled(boolean scheduled) {
+        this.scheduled = scheduled;
+    }
 
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
-
+            synchronized (lock) {
+                while (!scheduled) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                // implementation
+                if (this.checkValidCombination()) {
+                    this.assembleDrone();
+                }
+                // when done logic
+                this.scheduled = false;
+            }
         }
 
     }
 
-    @Override
-    public void tableUpdated() {
-        if (checkValidCombination()) {
-
-        } else {
-            // this.wait(); must be lock.wait()
-        }
+    private void assembleDrone() {
+        agent.incrementCount();
     }
 
     private boolean checkValidCombination() {
-        return true;
+        synchronized (lock) {
+            ArrayList<Components> assemblyTable = agent.getAssemblyTable();
+            for (Components component : assemblyTable) {
+                if (component.equals(inheritedComponent)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 }
