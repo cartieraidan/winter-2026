5c8c7c062ca9c97efaa689c716d25e25ed9a4c30
cartieraidan <cartieraidan@gmail.com>
2026-01-12 17:26:43 -0500

    Refactor Technician class to implement ObservableTech

--- SYSC3303/lab1/Technician.java
+++ SYSC3303/lab1/Technician.java
@@ -4,15 +4,26 @@
  * @author Aidan Cartier
  * @version Jan 12, 2026
  */
-public class Technician extends Thread implements observable {
+public class Technician extends Thread implements ObservableTech {
 
     @Override
     public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+
+        }
 
     }
 
     @Override
     public void tableUpdated() {
+        if (checkValidCombination()) {
 
+        } else {
+            // this.wait(); must be lock.wait()
+        }
+    }
+
+    private boolean checkValidCombination() {
+        return true;
     }
 }
