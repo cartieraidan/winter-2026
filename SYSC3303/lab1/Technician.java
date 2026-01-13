e964b7ec9afd78dc2defe68ab2ce3eae8345b3ee
cartieraidan <cartieraidan@gmail.com>
2026-01-12 19:02:31 -0500

    Update Technician.java

--- SYSC3303/lab1/Technician.java
+++ SYSC3303/lab1/Technician.java
@@ -11,12 +11,16 @@ public class Technician extends Thread  {
     private boolean scheduled = false;
     private Components inheritedComponent;
     private final Agent agent;
-    private final Object lock;
+    private final Object lock1;
+    private final Object lock2;
+    private String name;
 
-    public Technician(Components inheritedComponent, Agent agent) {
+    public Technician(Components inheritedComponent, Agent agent,  String name) {
+        this.name = name;
         this.inheritedComponent = inheritedComponent;
         this.agent = agent;
-        this.lock = agent.getLock();
+        this.lock1 = agent.getLock1();
+        this.lock2 = agent.getLock2();
     }
 
     public void setScheduled(boolean scheduled) {
@@ -26,10 +30,10 @@ public class Technician extends Thread  {
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
-            synchronized (lock) {
+            synchronized (lock1) {
                 while (!scheduled) {
                     try {
-                        lock.wait();
+                        lock1.wait();
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                         throw new RuntimeException(e);
@@ -37,10 +41,13 @@ public class Technician extends Thread  {
                     }
                 }
                 // implementation
+                System.out.println(name + " Scheduled");
                 if (this.checkValidCombination()) {
+                    System.out.println(name + " is valid");
                     this.assembleDrone();
                 }
                 // when done logic
+                agent.incrementLatch();
                 this.scheduled = false;
             }
         }
@@ -48,11 +55,13 @@ public class Technician extends Thread  {
     }
 
     private void assembleDrone() {
-        agent.incrementCount();
+        synchronized (lock1) {
+            agent.incrementCount();
+        }
     }
 
     private boolean checkValidCombination() {
-        synchronized (lock) {
+        synchronized (lock1) {
             ArrayList<Components> assemblyTable = agent.getAssemblyTable();
             for (Components component : assemblyTable) {
                 if (component.equals(inheritedComponent)) {
